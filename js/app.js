d61ac803d54dbece49d2d4c3eaedbba1205bca66
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-06 13:43:00 +0100

    ADD: ciclo for per stampare in console numeri da 1 a 100

--- js/app.js
+++ js/app.js
@@ -3,4 +3,10 @@
 // per i multipli di 5 stampi “Buzz” al post del numero.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
 
-// 
+// Scrivo un programma, usando ciclo for, per stampare numeri da 1 a 100//
+
+let n = 100; //number
+
+for(let i = 1; i <= n; ++i){
+    console.log(i)
+}
