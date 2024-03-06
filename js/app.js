6887aa2bf227981840f6bea91d6ea21edd1a18f2
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-06 13:39:16 +0100

    ADD: struttura iniziale esercizio

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// --- Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
+// per i multipli di 3 stampi “Fizz” al posto del numero e
+// per i multipli di 5 stampi “Buzz” al post del numero.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+
+// 
