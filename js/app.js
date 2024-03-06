1d64467d01efbb3f3caff90a148020c1891921ec
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-06 15:32:41 +0100

    ADD: con inner.HTML ho aggiunto elemento <li> con fantasia per tipo

--- js/app.js
+++ js/app.js
@@ -9,19 +9,35 @@ let n = 100; //number
 
 //Uso ciclo for per assegnare valori ai multipli di 3 e 5 
 
+
+// --- BONUS 1:
+// Crea un container nel DOM , aggiungendo un elemento html con il numero o la stringa corretta da mostrare.
+// Potete usare innerHTML oppure il metodo append creando l’elemento come oggetto usando createElement
+
+// BONUS 2:
+// Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, 
+// a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+
+let ulList = document.getElementById('ul-list'); //element
+
+
 for(let i = 1; i <= n; ++i){
 
-    let x3 = i % 3;
-    let x5 = i % 5;
-    let x15 = i % 15;
+   
+
+    let x3 = i % 3; //number
+    let x5 = i % 5; //numer
+    let x15 = i % 15; //number
     
     if(x15 === 0){
-        console.log('FizzBuzz')
+        ulList.innerHTML += `<li class="list-item fizzbuzz">FizzBuzz</li>`
     } else if (x5 === 0){
-        console.log('Buzz')
+        ulList.innerHTML += `<li class="list-item buzz">Buzz</li>`
     } else if (x3 === 0){
-        console.log('Fizz')
+        ulList.innerHTML += `<li class="list-item fizz">Fizz</li>`
     } else{
-        console.log(i)
+        ulList.innerHTML += `<li class="list-item number">${i}</li>`
     }
 }
+
+
